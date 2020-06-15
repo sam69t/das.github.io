753e20029b72f88e4d8b1eb0c019954efd9fdc7b
sam69t <samueldumezz@yahoo.fr>
2020-06-15 15:25:26 +0200

    change

--- js/script.js
+++ js/script.js
@@ -24,6 +24,20 @@ window.addEventListener('load', function() {
       next: '.vertical-blue-1 .glider-next'
     }
   });
-  
 
-});
+  window.takeScreenShot = function() {
+      html2canvas(document.querySelector(" .vertical-blue")).then(canvas => {
+      // document.body.appendChild(canvas);
+       var link = document.createElement('a');
+    link.download = 'filename.png';
+    link.href = canvas.toDataURL();
+    link.click();
+  });
+  }
+
+
+  let btn_screen = document.querySelector('#screenshot');
+  btn_screen.addEventListener('click', window.takeScreenShot);
+
+
+  });
